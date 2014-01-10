4c4b0540ac0444bc818bf106ad3c4b6e4845cd42
dezal <dez@gmx.at>
2014-01-10 22:01:31 +0100

    Futtersuche und Pheromon Suche weiterprogrammiert. Pheromonvergleich und Random Richtung  ist noch ausständig

--- AntHill/cAnt.cpp
+++ AntHill/cAnt.cpp
@@ -5,12 +5,14 @@
 #include <typeinfo>
 
 
-cAnt::cAnt(cField* position):ActualPosition(position),typ(3),TTL((LINES+COLUMNS+(LINES+COLUMNS)/2)*3),Lunchbox(0)//Joey: Lunchbox muss noch ausgearbeitet werden!
+cAnt::cAnt(cField* position):ActualPosition(position),typ(3),TTL((LINES+COLUMNS+(LINES+COLUMNS)/2)*3),Lunchbox(100)//Joey: Lunchbox muss noch ausgearbeitet werden!
 {
 	Fund=0;
 	Status = true; //true = Futtersuche/ false = Way Home
 	ActualPosition->adItem(this);
 	carrymehomelist.push_front(ActualPosition);
+	Lunchbox=100;
+	
 }
 
 
@@ -29,6 +31,12 @@ Fund=NULL;
 //accessors
 void cAnt::act()
 {
+	phNorth=0; //Pheromonstatus im Norden, wenn true dann Pheromon vorhanden wenn false dann keines
+	phEast=0;
+	phSouth=0;
+	phWest=0;
+
+
 	//std::cout<<"SIR. Ich bin eine Ameise!, SIR."<<std::endl; 
 	
 	//PART 1:
@@ -42,13 +50,13 @@ void cAnt::act()
 	}
 	else //THE SHOW MUST GO ON! Proviantstatus -- und Lebensstatus --
 	{
-		Lunchbox--,//Joey: Der Umgang mit der Lunchbox ist noch nicht zu ende gedacht!!!
+		Lunchbox--;//Joey: Der Umgang mit der Lunchbox ist noch nicht zu ende gedacht!!!
 			TTL--;
 	}
 
 	//PART 2:
 		//Futtersuche oder am Heimweg?
-		//Wenn auf Futtersuche, dann 
+		//Wenn auf Futtersuche, danpn 
 				//ist auf meinem Feld der Ameisenhügel? wenn ja, dann was ist sonst um mich herum?
 				//Ist Futter auf meinem Feld? 
 					//wenn Futter, dann Futter aufnehmen und Status auf Home! 
@@ -59,7 +67,7 @@ void cAnt::act()
 						//wenn Pheromon, dann dorthin gehen
 						//sonst Random nach LROU (falls Anthill, hier mit Parameter mitgeben auf welchem der Felder (north=1, east=2, south=3 west=4), um es auszuschließen) 
 
-	if(Status) //Futtersuche
+	if(Status) //Futtersuche : Ist relativ aufwendig geworden, wenn in irgenteiner Richtung futter gefunden wird, wird dort hinge´gangen.
 	{
 		if(!ActualPosition->getTypamount(2))//Ist auf meinem aktuellen Feld der Anthill? - NOPE, dann
 		{
@@ -69,6 +77,7 @@ void cAnt::act()
 			}
 			else//Was ist sonst um mich herum
 			{
+//Suche im norden
 				if(!ActualPosition->getpNorth.getTypamount(2))//Ist im Norden der Ameisenhügel? - Nope, dann
 				{
 					if(!ActualPosition->getpNorth.getTypamount(1))//Ist dort Futter? - Nope, dann
@@ -76,18 +85,325 @@ void cAnt::act()
 						if(!ActualPosition->getpNorth.getTypamount(4))//Ist dort Pheromon? - Nope, dann
 						{
 
+							
+//Suche im osten
+							if(!ActualPosition->getpEast.getTypamount(2))//Ist im osten der Ameisenhügel? - Nope, dann
+							{
+								if(!ActualPosition->getpEast.getTypamount(1))//Ist dort Futter? - Nope, dann
+								{
+									if(!ActualPosition->getpEast.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+									{
+//Suche im süden
+														if(!ActualPosition->getpSouth.getTypamount(2))//Ist im süden der Ameisenhügel? - Nope, dann
+														{
+															if(!ActualPosition->getpSouth.getTypamount(1))//Ist dort Futter? - Nope, dann
+															{
+																if(!ActualPosition->getpSouth.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																{
+//Suche im Westen
+																						if(!ActualPosition->getpWest.getTypamount(2))//Ist im Wsten der Ameisenhügel? - Nope, dann
+																						{
+																							if(!ActualPosition->getpWest.getTypamount(1))//Ist dort Futter? - Nope, dann
+																							{
+																								if(!ActualPosition->getpWest.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																								{
+
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+
+
+																								}else//Wenn im Wsten ein Pheromon, dann 
+																								{
+																									phWest=1;
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}
+																							}else //Wenn im westen Futter, Dann setze Actuelle  Pos dorthin
+																							{
+																								ActualPosition = ActualPosition->getpWest;
+																								carrymehomelist.push_back(ActualPosition);
+																							}
+																						}
+
+																}else//Wenn im Süden ein Pheromon, dann setze phSouth auf true und suche im Westen weiter 
+																{
+																	phSouth=1;
+//Suche im Westen
+																						if(!ActualPosition->getpWest.getTypamount(2))//Ist im Wsten der Ameisenhügel? - Nope, dann
+																						{
+																							if(!ActualPosition->getpWest.getTypamount(1))//Ist dort Futter? - Nope, dann
+																							{
+																								if(!ActualPosition->getpWest.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																								{
+																											ActualPosition = Whatsnext();
+																											carrymehomelist.push_back(ActualPosition);
+
+																								}else//Wenn im Wsten ein Pheromon, dann 
+																								{
+																									phWest=1;
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}
+																							}else //Wenn im westen Futter, Dann setze Actuelle  Pos dorthin
+																							{
+																								ActualPosition = ActualPosition->getpWest;
+																								carrymehomelist.push_back(ActualPosition);
+																							}
+																						}
+																}
+															}else //Wenn im Süden Futter, Dann setze Actuelle  Pos dorthin
+															{
+																ActualPosition = ActualPosition->getpSouth;
+																carrymehomelist.push_back(ActualPosition);
+															}
+				
+
+									}else//Wenn im osten ein Pheromon, dann setze phEast auf true und suche weiter im Süden
+									{
+										phEast=1;
+
+//Suche im süden0
+															if(!ActualPosition->getpSouth.getTypamount(2))//Ist im süden der Ameisenhügel? - Nope, dann
+															{
+																if(!ActualPosition->getpSouth.getTypamount(1))//Ist dort Futter? - Nope, dann
+																{
+																	if(!ActualPosition->getpSouth.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																	{
+//Suche im Westen0
+																						if(!ActualPosition->getpWest.getTypamount(2))//Ist im Wsten der Ameisenhügel? - Nope, dann
+																						{
+																							if(!ActualPosition->getpWest.getTypamount(1))//Ist dort Futter? - Nope, dann
+																							{
+																								if(!ActualPosition->getpWest.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																								{
+
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}else//Wenn im Wsten ein Pheromon, dann 
+																								{
+																									phWest=1;
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}
+																							}else //Wenn im westen Futter, Dann setze Actuelle  Pos dorthin
+																							{
+																								ActualPosition = ActualPosition->getpWest;
+																								carrymehomelist.push_back(ActualPosition);
+																							}
+																						}
+
+																	}else//Wenn im Süden ein Pheromon, dann setze phSouth auf true und suche im Westen weiter
+																	{
+																		phSouth=1;
+//Suche im Westen0
+																						if(!ActualPosition->getpWest.getTypamount(2))//Ist im Wsten der Ameisenhügel? - Nope, dann
+																						{
+																							if(!ActualPosition->getpWest.getTypamount(1))//Ist dort Futter? - Nope, dann
+																							{
+																								if(!ActualPosition->getpWest.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																								{
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+
+																								}else//Wenn im Wsten ein Pheromon, dann 
+																								{
+																									phWest=1;
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}
+																							}else //Wenn im westen Futter, Dann setze Actuelle  Pos dorthin
+																							{
+																								ActualPosition = ActualPosition->getpWest;
+																								carrymehomelist.push_back(ActualPosition);
+																							}
+																						}
+																	}
+																}else //Wenn im Süden Futter, Dann setze Actuelle  Pos dorthin
+																{
+																	ActualPosition = ActualPosition->getpSouth;
+																	carrymehomelist.push_back(ActualPosition);
+																}
+															}
+									}
+								}else //Wenn im osten Futter, Dann setze Actuelle  Pos dorthin
+								{
+									ActualPosition = ActualPosition->getpEast;
+									carrymehomelist.push_back(ActualPosition);
+								}
+							}
+
+						}else//Wenn im norden ein Pheromon, dann setze phNorth true und suche im osten weiter
+						{
+						phNorth=1;
+
+//Suche im osten0
+
+							if(!ActualPosition->getpEast.getTypamount(2))//Ist im osten der Ameisenhügel? - Nope, dann
+							{
+								if(!ActualPosition->getpEast.getTypamount(1))//Ist dort Futter? - Nope, dann
+								{
+									if(!ActualPosition->getpEast.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+									{
+//Suche im süden0
+															if(!ActualPosition->getpSouth.getTypamount(2))//Ist im süden der Ameisenhügel? - Nope, dann
+															{
+																if(!ActualPosition->getpSouth.getTypamount(1))//Ist dort Futter? - Nope, dann
+																{
+																	if(!ActualPosition->getpSouth.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																	{
+//Suche im Westen0
+																						if(!ActualPosition->getpWest.getTypamount(2))//Ist im Wsten der Ameisenhügel? - Nope, dann
+																						{
+																							if(!ActualPosition->getpWest.getTypamount(1))//Ist dort Futter? - Nope, dann
+																							{
+																								if(!ActualPosition->getpWest.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																								{
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+
+																								}else//Wenn im Wsten ein Pheromon, dann 
+																								{
+																									phWest=1;
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}
+																							}else //Wenn im westen Futter, Dann setze Actuelle  Pos dorthin
+																							{
+																								ActualPosition = ActualPosition->getpWest;
+																								carrymehomelist.push_back(ActualPosition);
+																							}
+																						}
+
+																	}else//Wenn im Süden ein Pheromon, dann setze phSouth auf true und suche im Westen weiter
+																	{
+																		phSouth=1;
+//Suche im Westen
+																						if(!ActualPosition->getpWest.getTypamount(2))//Ist im Wsten der Ameisenhügel? - Nope, dann
+																						{
+																							if(!ActualPosition->getpWest.getTypamount(1))//Ist dort Futter? - Nope, dann
+																							{
+																								if(!ActualPosition->getpWest.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																								{
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}else//Wenn im Wsten ein Pheromon, dann 
+																								{
+																									phWest=1;
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}
+																							}else //Wenn im westen Futter, Dann setze Actuelle  Pos dorthin
+																							{
+																								ActualPosition = ActualPosition->getpWest;
+																								carrymehomelist.push_back(ActualPosition);
+																							}
+																						}
+																	}
+																}else //Wenn im Süden Futter, Dann setze Actuelle  Pos dorthin
+																{
+																	ActualPosition = ActualPosition->getpSouth;
+																	carrymehomelist.push_back(ActualPosition);
+																}
+															}
+									}
+
+									}else//Wenn im osten ein Pheromon, dann setze phEast auf true und suche weiter im Süden
+									{
+										phEast=1;
+//Suche im süden0
+															if(!ActualPosition->getpSouth.getTypamount(2))//Ist im süden der Ameisenhügel? - Nope, dann
+															{
+																if(!ActualPosition->getpSouth.getTypamount(1))//Ist dort Futter? - Nope, dann
+																{
+																	if(!ActualPosition->getpSouth.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																	{
+//Suche im Westen0
+																						if(!ActualPosition->getpWest.getTypamount(2))//Ist im Wsten der Ameisenhügel? - Nope, dann
+																						{
+																							if(!ActualPosition->getpWest.getTypamount(1))//Ist dort Futter? - Nope, dann
+																							{
+																								if(!ActualPosition->getpWest.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																								{
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}else//Wenn im Wsten ein Pheromon, dann 
+																								{
+																									phWest=1;
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}
+																							}else //Wenn im westen Futter, Dann setze Actuelle  Pos dorthin
+																							{
+																								ActualPosition = ActualPosition->getpWest;
+																								carrymehomelist.push_back(ActualPosition);
+																							}
+																						}
+
+																	}else//Wenn im Süden ein Pheromon, dann setze phSouth auf true und suche im Westen weiter
+																	{
+																		phSouth=1;
+//Suche im Westen0
+																						if(!ActualPosition->getpWest.getTypamount(2))//Ist im Wsten der Ameisenhügel? - Nope, dann
+																						{
+																							if(!ActualPosition->getpWest.getTypamount(1))//Ist dort Futter? - Nope, dann
+																							{
+																								if(!ActualPosition->getpWest.getTypamount(4))//Ist dort Pheromon? - Nope, dann
+																								{
+
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}else//Wenn im Wsten ein Pheromon, dann 
+																								{
+																									phWest=1;
+																									ActualPosition = Whatsnext();
+																									carrymehomelist.push_back(ActualPosition);
+																								}
+																							}else //Wenn im westen Futter, Dann setze Actuelle  Pos dorthin
+																							{
+																								ActualPosition = ActualPosition->getpWest;
+																								carrymehomelist.push_back(ActualPosition);
+																							}
+																						}
+				
+																	}
+																}else //Wenn im Süden Futter, Dann setze Actuelle  Pos dorthin
+																{
+																	ActualPosition = ActualPosition->getpSouth;
+																	carrymehomelist.push_back(ActualPosition);
+																}
+															}
+									}
+									
+								}else //Wenn im osten Futter, Dann setze Actuelle  Pos dorthin
+								{
+									ActualPosition = ActualPosition->getpEast;
+									carrymehomelist.push_back(ActualPosition);
+								}
+							}
 						}
+					}else //Wenn im norden Futter, Dann setze Actuelle  Pos dorthin
+					{
+						ActualPosition = ActualPosition->getpNorth;
+						carrymehomelist.push_back(ActualPosition);
 					}
 				}
+				
+
+				
+
+				
 			}
+		}else //Wenn Ant auf Anthill dann wird Lunchbox aufgefüllt.
+		{
+			Lunchbox=100;
 		}
-	}
-	if(Status)
-	{
-		//Futter=333; Pheromon= >0 && <101; Nichts= 0; Anthill=222;
-		ActualPosition = Whatsnext();
-		carrymehomelist.push_back(ActualPosition);
-	}
+	
+	//if(true)
+	//{
+	//	//Futter=333; Pheromon= >0 && <101; Nichts= 0; Anthill=222;
+	//	ActualPosition = Whatsnext();
+	//	carrymehomelist.push_back(ActualPosition);
+	//}
 
 	//Futtersuche oder am Heimweg?
 		//Wenn am Heimweg, dann 
@@ -97,8 +413,12 @@ void cAnt::act()
 			//Homelist abarbeiten!
 
 
-	else //Weg heim
+	}else //Weg heim
 	{
+		
+
+
+		//Homelist abarbeiten
 		ActualPosition->remItem(this);
 		ActualPosition= carrymehomelist.back();
 		carrymehomelist.pop_back();
@@ -136,11 +456,37 @@ void cAnt::act()
 					//sonst Random nach LROU (falls Anthill, hier mit Parameter mitgeben auf welchem der Felder (north=1, east=2, south=3 west=4), um es auszuschließen)  
 
 	
-	cField*	cAnt::Whatsnext()
+	cField*	cAnt::Whatsnext() // TDOO: es muss noch verglichen werden wenn mehrere Pheromone da sind.
 	{
-		//Futter=333; Pheromon= >0 && <101; Nichts= 0; Anthill=222;
-		ActualPosition->getpNorth
-		if(typeid().name ==typeid(cFood).name)
-		ActualPosition->getpNorth
+		////Futter=333; Pheromon= >0 && <101; Nichts= 0; Anthill=222;
+		//ActualPosition->getpNorth
+		//if(typeid().name ==typeid(cFood).name)
+		//ActualPosition->getpNorth
+
+		if(!phNorth && !phEast && !phSouth && !phWest)
+		{	
+			return randway();
+		}else if(phNorth && !phEast && !phSouth && !phWest)
+		{
+			return ActualPosition->getpNorth;
+
+		}else if(!phNorth && phEast && !phSouth && !phWest)
+		{
+			return ActualPosition->getpEast;
+
+		}else if(!phNorth && !phEast && phSouth && !phWest)
+		{
+			return ActualPosition->getpSouth;
+
+		}else if(!phNorth && !phEast && !phSouth && phWest)
+		{
+			return ActualPosition->getpWest;
+		}
+	}
+
+
+	cField* cAnt::randway() // gibt eine Zufallsrichtung zurück! muss noch ausprogrammiert werden!
+	{
+
 
 	}
--- AntHill/cAnt.h
+++ AntHill/cAnt.h
@@ -15,11 +15,17 @@ private:
 	int typ;
 	int Lunchbox;//Joey: Der Umgang mit der Lunchbox ist noch nicht zu ende gedacht!!!
 	int TTL;
+	bool phNorth;
+	bool phEast;
+	bool phSouth;
+	bool phWest;
+
 	bool Status;
 	std::list<cField*>carrymehomelist;
 	cItem* Fund;
 	cField* ActualPosition;
 	cField* Whatsnext();
+	cField*	randway(); 
 public:
 	//cAnt(void);//Joey: Ich versuche den Konstruktor mal so umzuschreiben, dass die Ameise beim Entstehen ihre momentane Lage mitgegeben bekommt!
 	cAnt(cField* ActualPosition);
